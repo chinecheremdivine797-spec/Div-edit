d5a264fac0b8699152b2b46dc00ead62992bf9b5
chinecheremdivine797-spec <chinecheremdivine797@gmail.com>
2026-09-13 13:24:29 +0100

    Fix screenshot test SDK compatibility

--- app/src/test/java/com/example/GreetingScreenshotTest.kt
+++ app/src/test/java/com/example/GreetingScreenshotTest.kt
@@ -14,15 +14,11 @@ import org.robolectric.annotation.GraphicsMode
 
 @RunWith(RobolectricTestRunner::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
-@Config(qualifiers = RobolectricDeviceQualifiers.Pixel8, sdk = [36])
+@Config(qualifiers = RobolectricDeviceQualifiers.Pixel8, sdk = [35])
 class GreetingScreenshotTest {
-
   @get:Rule val composeTestRule = createComposeRule()
-
-  @Test
-  fun greeting_screenshot() {
+  @Test fun greeting_screenshot() {
     composeTestRule.setContent { MyApplicationTheme { Greeting("Robolectric") } }
-
     composeTestRule.onRoot().captureRoboImage(filePath = "src/test/screenshots/greeting.png")
   }
 }
