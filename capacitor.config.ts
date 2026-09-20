ea9b5485857357fd5012435270085f59438ba014
chinecheremdivine797-spec <chinecheremdivine797@gmail.com>
2026-09-20 19:18:18 +0100

    Fix Capacitor config ESM type import

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ng.divstudio.magiceditor',
   appName: 'Magic Editor',
   webDir: 'dist',
-  bundledWebRuntime: false,
 };
 
 export default config;
